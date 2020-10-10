45a1fd3cbdff7abb1cae63b5da4e7ff4a04c4a25
Saroj Basnet <54398451+saroj-sudo@users.noreply.github.com>
2020-10-10 22:05:44 +0545

    new look

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,91 +7,72 @@ function Calculator() {
   [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ".", "%"].forEach((item) => {
     calcBtns.push(
       <button
-        value={item}
-        key={item}
         onClick={(e) => {
           setInput(input + e.target.value);
         }}
+        value={item}
+        key={item}
       >
+        {" "}
         {item}
       </button>
     );
   });
 
   return (
-    <div className="calculator">
-      <div>
-        <div className="input">{input}</div>
-      </div>
+    <div className="wrapper">
+      <div className="show-input">{input}</div>
+      <div className="digits">{calcBtns}</div>
+      <div className="modifiers subgrid">
+        {/* clear button */}
 
-      <button
-        className="clear_button"
-        onClick={() => {
-          setInput(input.substr(0, input.length - 1));
-        }}
-      >
-        Clear
-      </button>
-      <button className="clear_all_button" onClick={() => setInput("")}>
-        AC
-      </button>
+        <button onClick={() => setInput(input.substr(0, input.length - 1))}>
+          Clear
+        </button>
 
-      <div className="button_container">
-        <div>{calcBtns}</div>
+        {/* clear all */}
+        <button onClick={() => setInput("")} value="">
+          AC
+        </button>
       </div>
-      <div>
-        <button
-          value="+"
-          onClick={(e) => {
-            setInput(input + e.target.value);
-          }}
-        >
+      <div className="operations subgrid">
+        {/* add button */}
+        <button onClick={(e) => setInput(input + e.target.value)} value="+">
           +
         </button>
-        <button
-          value="-"
-          onClick={(e) => {
-            setInput(input + e.target.value);
-          }}
-        >
+
+        {/* minus btn */}
+        <button onClick={(e) => setInput(input + e.target.value)} value="-">
           -
         </button>
-        <button
-          value="*"
-          onClick={(e) => {
-            setInput(input + e.target.value);
-          }}
-        >
-          x
+
+        <button onClick={(e) => setInput(input + e.target.value)} value="*">
+          *
         </button>
-        <button
-          value="/"
-          onClick={(e) => {
-            setInput(input + e.target.value);
-          }}
-        >
+
+        <button onClick={(e) => setInput(input + e.target.value)} value="/">
           /
         </button>
+        {/* "=" btn */}
+        <button
+          onClick={(e) => {
+            try {
+              setInput(
+                String(eval(input)).length > 3 &&
+                  String(eval(input)).includes(".")
+                  ? String(eval(input).toFixed(4))
+                  : String(eval(input))
+              );
+            } catch (e) {
+              console.log(e);
+            }
+          }}
+          value="="
+        >
+          =
+        </button>
       </div>
-      <button
-        value="="
-        onClick={() => {
-          try {
-            setInput(
-              String(eval(input)).length > 2 &&
-                String(eval(input)).includes(".")
-                ? String(eval(input).toFixed(3))
-                : String(eval(input))
-            );
-          } catch (e) {
-            console.log(e);
-          }
-        }}
-      >
-        =
-      </button>
     </div>
   );
 }
-
 export default Calculator;
